b12945831baa5905c91aa1cfff40f899ce1c9906
charliekaks <kaksunchained@gmail.com>
2018-07-23 14:04:41 +0300

    removing configuration files

--- config/database.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = {
-        mongoURI: 'mongodb://charlie:qwerty101ck@ds143511.mlab.com:43511/node-prod'
-    }
-} else {
-    module.exports ={
-        mongoURI:"mongodb://localhost:27017/node-app"
-    }
-}
